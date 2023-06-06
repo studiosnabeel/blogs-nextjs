5f5dba83391a8a49f8f0da94cefb8a022ed2d137
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-06 23:58:14 +0500

    Add content and styling for blog

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
+import styles from './page.module.css';
+import Image from 'next/image';
 
 const BlogPost = () => {
-  return <div>BlogPost</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Perferendis, illum!
+          </h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quae
+            repellat minus accusamus! Ipsum, pariatur, impedit minima modi
+            temporibus odio doloribus aut debitis consectetur natus recusandae
+            alias provident accusantium illum.
+          </p>
+          <div className={styles.author}>
+            <Image
+              src="/parrot.jpg"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>Nabeel Akbar</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/parrot.jpg"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium,
+          autem commodi. Iusto officia eaque amet itaque molestias quasi
+          blanditiis, tenetur ipsam? Aut culpa, aliquid repellendus cumque minus
+          nostrum ut dolores.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default BlogPost;
