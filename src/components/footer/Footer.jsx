2761a225205aac5f13643abb5352a601e5c02e53
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-06 16:12:06 +0500

    adding modular css to components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import styles from './footer.module.css';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>&copy;2023 NextBlogs. All rights reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          alt="Facebook icon"
+          width={15}
+          height={15}
+          className={styles.icon}
+        />
+        <Image
+          src="/2.png"
+          alt="Instagram icon"
+          width={15}
+          height={15}
+          className={styles.icon}
+        />
+        <Image
+          src="/3.png"
+          alt="Twitter icon"
+          width={15}
+          height={15}
+          className={styles.icon}
+        />
+        <Image
+          src="/4.png"
+          alt="Youtube icon"
+          width={15}
+          className={styles.icon}
+          height={15}
+        />
+      </div>
     </div>
   );
 };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,8 @@
+'use client';
+
 import React from 'react';
 import Link from 'next/link';
+import styles from './navbar.module.css';
 
 const links = [
   {
@@ -36,14 +39,24 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
-      <Link href="/">NextBlogs</Link>
-      <div>
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>
+        NextBlogs
+      </Link>
+      <div className={styles.links}>
         {links.map((link) => (
-          <Link key={link.id} href={link.url}>
+          <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
           </Link>
         ))}
+        <button
+          className={styles.logout}
+          onClick={() => {
+            console.log('logged out');
+          }}
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
