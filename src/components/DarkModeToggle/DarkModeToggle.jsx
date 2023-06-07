c21d6d655f81446ccf4c4b91989e50ea64fdfa23
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-07 14:55:26 +0500

    use toggle and mode in children

--- src/components/DarkModeToggle/DarkModeToggle.jsx
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+'use client';
+
+import React, { useContext } from 'react';
 import styles from './DarkModeToggle.module.css';
+import { ThemeContext } from '../../../context/ThemeContext';
 
 const DarkModeToggle = () => {
+  const { toggle, mode } = useContext(ThemeContext);
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggle}>
       <div className={styles.icon}>🌙</div>
       <div className={styles.icon}>🔆</div>
-      <div className={styles.ball} />
+      <div
+        className={styles.ball}
+        style={mode === 'light' ? { left: '2px' } : { right: '2px' }}
+      />
     </div>
   );
 };
