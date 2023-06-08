5877a06e2ee11f33b6580f4a08528c0b8698e7e5
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-08 18:17:33 +0500

    authenticate google

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import styles from './page.module.css';
 import useSWR from 'swr';
+import { useSession } from 'next-auth/react';
 
 const Dashboard = () => {
   // const [data, setData] = useState([]);
@@ -28,13 +29,17 @@ const Dashboard = () => {
   //   getData();
   // }, []);
 
+  const session = useSession();
+
+  console.log(session);
+
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
   const { data, error, isLoading } = useSWR(
     'https://jsonplaceholder.typicode.com/posts',
     fetcher
   );
-  console.log(data);
+  // console.log(data);
 
   return <div className={styles.container}>Dashboard</div>;
 };
