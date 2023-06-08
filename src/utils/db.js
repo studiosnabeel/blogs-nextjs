bd6e061d9a35e7f81b3dd6fd591822f741e42601
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-08 12:20:04 +0500

    Set mongoose to strict query

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 
 const connect = async () => {
+  mongoose.set('strictQuery', true);
+
   try {
     await mongoose.connect(process.env.MONGO);
   } catch (error) {
-    throw new Error('connection failed');
+    throw new Error('Connection failed!');
   }
 };
 
