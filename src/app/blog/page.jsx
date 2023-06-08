cc4db5792a8b6952cd440781b466d29c0a523db5
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-08 12:19:18 +0500

    Modify Blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import styles from './page.module.css';
-import Link from 'next/link';
-import Image from 'next/image';
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 async function getData() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
-    cache: 'no-store',
+  const res = await fetch("http://localhost:3000/api/posts", {
+    cache: "no-store",
   });
 
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
 
   return res.json();
@@ -18,23 +17,22 @@ async function getData() {
 
 const Blog = async () => {
   const data = await getData();
-
   return (
     <div className={styles.mainContainer}>
       {data.map((item) => (
-        <Link href="/blog/testId" className={styles.container} key={item.id}>
+        <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
           <div className={styles.imageContainer}>
             <Image
-              src="/deer.jpg"
+              src={item.img}
               alt=""
-              width={250}
-              height={200}
+              width={400}
+              height={250}
               className={styles.image}
             />
           </div>
           <div className={styles.content}>
             <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.desc}>{item.body}</p>
+            <p className={styles.desc}>{item.desc}</p>
           </div>
         </Link>
       ))}
