7e91b95c2c58ae22f61633768d0e66d74d5673f7
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-09 18:31:16 +0500

    useRouter in dashboard page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import styles from './page.module.css';
 import useSWR from 'swr';
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const Dashboard = () => {
   // const [data, setData] = useState([]);
@@ -31,7 +32,7 @@ const Dashboard = () => {
 
   const session = useSession();
 
-  console.log(session);
+  const router = useRouter();
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -39,9 +40,18 @@ const Dashboard = () => {
     'https://jsonplaceholder.typicode.com/posts',
     fetcher
   );
-  // console.log(data);
 
-  return <div className={styles.container}>Dashboard</div>;
+  if (session.status === 'loading') {
+    return <p>Loading...</p>;
+  }
+
+  if (session.status === 'unauthenticated') {
+    router?.push('/dashboard/login');
+  }
+
+  if (session.status === 'authenticated') {
+    return <div className={styles.container}>Dashboard</div>;
+  }
 };
 
 export default Dashboard;
