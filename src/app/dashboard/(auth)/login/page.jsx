0003081edb9a4c1f7ededaf72ad2a77f60c45497
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-09 18:30:04 +0500

    Modify Login page

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -2,11 +2,41 @@
 
 import React from 'react';
 import styles from './page.module.css';
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const Login = () => {
+  const session = useSession();
+
+  const router = useRouter();
+
+  if (session.status === 'loading') {
+    return <p>Loading...</p>;
+  }
+
+  if (session.status === 'authenticated') {
+    router?.push('/dashboard');
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    signIn('credentials', { email, password });
+  };
   return (
     <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input type="email" placeholder="email" className={styles.input} />
+        <input
+          type="password"
+          placeholder="password"
+          className={styles.input}
+        />
+        <button className={styles.button}>Login</button>
+      </form>
       <button onClick={() => signIn('google')}>Login with Google</button>
     </div>
   );
