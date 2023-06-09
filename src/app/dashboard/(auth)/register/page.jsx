5c6c99a83133e918b4eedd2b8c64ef40310622e5
Nabeel Akbar <studiosnabeel@gmail.com>
2023-06-09 14:15:34 +0500

    Add content and styling to Register page

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,7 +1,54 @@
-import React from 'react';
+'use client';
+
+import Link from 'next/link';
+import React, { useState } from 'react';
+import styles from './page.module.css';
+import { useRouter } from 'next/navigation';
 
 const Register = () => {
-  return <div>Register</div>;
+  const router = useRouter();
+  const [err, setErr] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    try {
+      const res = await fetch('/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+
+      res.status === 201 && router.push('/dashboard/login?success=Account has been created');
+    } catch (err) {
+      setErr(true);
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input type="text" placeholder="username" className={styles.input} />
+        <input type="email" placeholder="email" className={styles.input} />
+        <input
+          type="password"
+          placeholder="password"
+          className={styles.input}
+        />
+        <button className={styles.button}>Register</button>
+      </form>
+      {err && 'something went wrong'}
+      <Link href="/dashboard/login">Login with an existing account</Link>
+    </div>
+  );
 };
 
 export default Register;
